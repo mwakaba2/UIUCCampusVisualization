1a82a9fdbca4e92163ac82661abe55dfcc9b92f9
Mariko Wakabayashi <marimarimekko@gmail.com>
2016-03-10 13:00:53 -0600

    function that takes in a point and returns labels

--- getLabels.js
+++ getLabels.js
@@ -1,19 +1,25 @@
-
-
 $.getJSON( "data/retrieved_data.json", function( data ) {
   var point = [1750, 925];
-  var pointX = point[0];
-  var pointY = point[1];
-  var labels = [];
-  for(var key in data){
-  	var object = data[key];
-	var rectangle = object["rectangle"];
-	var northwest = rectangle["coord"][0];
-	var southeast = rectangle["coord"][1];
 
-	if(northwest[0] < pointX && pointX < southeast[0] && northwest[1] < pointY && pointY < southeast[1]){
-		labels.push(rectangle["label"]);
-	}
+  var testLabels = getLabels(point);
+
+  function getLabels(point) {
+    var pointX = point[0];
+    var pointY = point[1];
+    var labels = [];
+    for(var key in data){
+      var object = data[key];
+      var rectangle = object["rectangle"];
+      var northwest = rectangle["coord"][0];
+      var southeast = rectangle["coord"][1];
+
+      if(northwest[0] < pointX && pointX < southeast[0] && northwest[1] < pointY && pointY < southeast[1]){
+        labels.push(rectangle["label"]);
+      }
+    }
+    return labels;
   }
-  console.log(labels);
+  
+  console.log(testLabels);
+
 });
