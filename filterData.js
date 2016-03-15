889fda011169534703570b76daf8873082e150cb
enbo <bezhang2@illinois.edu>
2016-03-15 07:10:35 -0500

    FINISHED
    YES

--- filterData.js
+++ filterData.js
@@ -7,12 +7,56 @@ $.getJSON( "data/retrieved_data.json", function( data ) {
   }
 });
 
-var point = [1750, 925];
-var testLabels = getLabels(point);
-var testColleges = ['ENGR'];
-var testSemesterRange = [5, 12];
-var testTransportation = ['bus'];
-var testKeyword = "Main Quad";
+function checkSemesters(user_semester) {
+  if (user_semester < sliderRange[0] || user_semester > sliderRange[1]) {
+    return false;
+  }
+  return true;
+}
+
+function checkVehicle(user_vehicles, vehicles) {
+  if (_.intersection(user_vehicles, vehicles).length > 0) {
+    return true;
+  }
+  return false;
+}
+
+function checkColleges(user_colleges, colleges) {
+  if (_.intersection(user_colleges, colleges).length > 0) {
+    return true;
+  }
+  return false;
+}
+
+function checkQuery(label, keyword) {
+  var re = new RegExp(keyword,"gi");
+  if (label.match(re) === null) {
+    return false;
+  }
+  return true;
+}
+
+function checkPoint(coord, p) {
+  var pointX = p[0];
+  var pointY = p[1];
+  var northwest = coord[0];
+  var southeast = coord[1];
+
+  if(northwest[0] < pointX && pointX < southeast[0] && northwest[1] < pointY && pointY < southeast[1]) {
+    return true;
+  }
+  return false;
+}
+
+function checkHomes(h, user_homes) {
+  for (var i in h) {
+    for (var j in user_homes)
+    if (checkPoint(h[i], user_homes[j])) {
+      return true;
+    }
+  }
+  return false;
+}
 
 /**
  * Get the aggregated labels of rectangles that have the keywords in the label
@@ -39,74 +83,145 @@ function getAggregate(count_dict){
   }
 }
 
+function recalculate() {
 
+  var keyword = $("input[name=keyword]").val();
+  var colleges = $('input[type="checkbox"][name="colleges"]:checked').map(function(){
+    return this.value;
+  }).toArray();
+  var vehicles = $('input[type="checkbox"][name="vehicles"]:checked').map(function(){
+    return this.value;
+  }).toArray();
+
+  var count_dict = {};
+  var rectangles = [];
+
+  for (var i in retrievedData) {
+    var thing = retrievedData[i];
+    var info = thing["user_info"];
+    var user_colleges = info["college"];
+    var user_semester = parseInt(info["semester"]);
+    var user_vehicles = info["transportation"];
+    if (user_vehicles.length === 0) {
+      user_vehicles.push("walkOnly");
+    }
+    var user_homes = info["house"];
+    var rectangle = thing["rectangle"];
+    var label = rectangle["label"].toLowerCase();
+    var coord = rectangle["coord"];
+
+    if(rectangle == undefined){
+      break;
+    }
+
+    var pass = true; // changes to false once rectangle fails a filter
+
+    pass = checkSemesters(user_semester);
+
+    if (pass && vehicles.length > 0) {
+      pass = checkVehicle(user_vehicles, vehicles);
+    }
+    if (pass && colleges.length > 0) {
+      pass = checkColleges(user_colleges, colleges);
+    }
+    if (pass && keyword !== "") {
+      pass = checkQuery(label, keyword);
+    }
+    if (pass && point.length > 0) {
+      pass = checkPoint(coord, point);
+    }
+    if (pass && homes.length > 0) {
+      pass = checkHomes(homes, user_homes);
+    }
+
+    if (pass) {
+      // labels.push(label);
+
+      if(!(label in count_dict))
+        count_dict[label] = 1
+      else
+        count_dict[label] += 1
+
+      rectangles.push(rectangle);
+    }
+
+  }
+
+  drawFog(rectangles);
+
+  drawFilter();
+
+  getAggregate(count_dict);
+
+  stage.update();
+}
 
 /**
  * Get the labels of rectangles that have the keywords in the label
  * @param {string} keywords
  * @return {array of strings} labels
  */
-function searchFilter(keywords) {
-  // $("#labels-list").empty();
-  var rectangles = [];
-  count_dict = {}
-  for(var key in retrievedData){
-    var object = retrievedData[key];
-    var rectangle = object["rectangle"];
-
-    if(rectangle == undefined){
-      break;
-    }
-    var label = rectangle["label"];
-
-    // case insensitive and global matchings
-    var re = new RegExp(keywords,"gi");
-    var matchings = label.match(re);
-    if(matchings){
-      label = label.toLowerCase()
-      if(!(label in count_dict))
-        count_dict[label] = 1
-      else
-        count_dict[label] += 1
-      rectangles.push(rectangle);
-    }
-  }
-
-  getAggregate(count_dict)
-
-
-  return rectangles;
-}
+// function searchFilter(keywords) {
+//   // $("#labels-list").empty();
+//   var rectangles = [];
+//   count_dict = {};
+//   for(var key in retrievedData){
+//     var object = retrievedData[key];
+//     var rectangle = object["rectangle"];
+//
+//     if(rectangle == undefined){
+//       break;
+//     }
+//     var label = rectangle["label"];
+//
+//     // case insensitive and global matchings
+//     var re = new RegExp(keywords,"gi");
+//     var matchings = label.match(re);
+//     if(matchings){
+//       label = label.toLowerCase();
+//       if(!(label in count_dict))
+//         count_dict[label] = 1
+//       else
+//         count_dict[label] += 1
+//       rectangles.push(rectangle);
+//     }
+//   }
+//
+//   getAggregate(count_dict)
+//
+//
+//   return rectangles;
+// }
 
 /**
  * Get the labels of rectangles that overlap with the point
  * @param {array of numbers} [x,y] The x and the y coordinate
  * @return {array of strings} labels
  */
-function getLabels(point) {
-  var pointX = point[0];
-  var pointY = point[1];
-  var labels = [];
-  var count_dict = {}
-  for(var key in retrievedData){
-    var dataObj = retrievedData[key];
-    var rectangle = dataObj["rectangle"];
-    var northwest = rectangle["coord"][0];
-    var southeast = rectangle["coord"][1];
-
-    if(northwest[0] < pointX && pointX < southeast[0] && northwest[1] < pointY && pointY < southeast[1]){
-      label = rectangle["label"]
-      labels.push(label);
-
-      if(!(label in count_dict))
-        count_dict[label] = 1
-      else
-        count_dict[label] += 1
-    }
-  }
-  getAggregate(count_dict)
-  return labels;
-}
+// function getLabels() {
+//   var pointX = point[0];
+//   var pointY = point[1];
+//   var labels = [];
+//   var count_dict = {}
+//   for(var key in retrievedData){
+//     var dataObj = retrievedData[key];
+//     var rectangle = dataObj["rectangle"];
+//     var northwest = rectangle["coord"][0];
+//     var southeast = rectangle["coord"][1];
+//
+//     if(northwest[0] < pointX && pointX < southeast[0] && northwest[1] < pointY && pointY < southeast[1]){
+//       label = rectangle["label"]
+//       labels.push(label);
+//
+//       if(!(label in count_dict))
+//         count_dict[label] = 1
+//       else
+//         count_dict[label] += 1
+//     }
+//   }
+//   getAggregate(count_dict)
+//   return labels;
+// }
 
 // setTimeout(function () {
 //     console.log(getLabels(point));
@@ -119,53 +234,53 @@ function getLabels(point) {
  * @param {array of strings} List of transportation
  * @return {array of Rectangles} Rectangle dataObjs
  */
- function filteredRectangles(colleges, semesterRange, transportation, homes) {
-  var rectangles = [];
-  count_dict = {}
-  for(var key in retrievedData){
-    var dataObj = retrievedData[key];
-    var usr_info = dataObj["user_info"];
-    var usr_colleges = usr_info["college"];
-    var usr_semesters = parseInt(usr_info["semester"]);
-    var usr_transportation = usr_info["transportation"];
-
-    if(colleges.length == 0 || isSameSet(colleges, usr_colleges)){
-      if(semesterRange[0] <= usr_semesters && usr_semesters <= semesterRange[1]){
-        if(transportation.length == 0 || isSameSet(transportation, usr_transportation)){
-          var usr_rectangle = dataObj["rectangle"];
-          var label = usr_rectangle["label"];
-          rectangles.push(usr_rectangle);
-          if(!(label in count_dict))
-            count_dict[label] = 1;
-          else
-            count_dict[label] += 1;
-        }
-      }
-    }
-  }
-  if(homes.length > 0){
-    for(var rect in rectangles){
-      var coords = rectangles[rec]["coord"];
-      var northwest = coords[0];
-      var southeast = coords[1];
-      for(var h in homes){
-        var hNW = homes[h][0];
-        var nSE = homes[h][1];
-        var overlapping = northwest[1] > hNW[1] && hNW[1] > southeast[1] || northwest[1] < hNW[1] && hSE[1] < northwest[1];
-        if(!overlapping){
-          rectangles.splice(rect, 1);
-        }
-      }
-    }
-  }
-  getAggregate(count_dict);
-  return rectangles;
- }
+ // function filteredRectangles(colleges, semesterRange, transportation, homes) {
+ //  var rectangles = [];
+ //  count_dict = {}
+ //  for(var key in retrievedData){
+ //    var dataObj = retrievedData[key];
+ //    var usr_info = dataObj["user_info"];
+ //    var usr_colleges = usr_info["college"];
+ //    var usr_semesters = parseInt(usr_info["semester"]);
+ //    var usr_transportation = usr_info["transportation"];
+ //
+ //    if(colleges.length == 0 || isSameSet(colleges, usr_colleges)){
+ //      if(semesterRange[0] <= usr_semesters && usr_semesters <= semesterRange[1]){
+ //        if(transportation.length == 0 || isSameSet(transportation, usr_transportation)){
+ //          var usr_rectangle = dataObj["rectangle"];
+ //          var label = usr_rectangle["label"];
+ //          rectangles.push(usr_rectangle);
+ //          if(!(label in count_dict))
+ //            count_dict[label] = 1;
+ //          else
+ //            count_dict[label] += 1;
+ //        }
+ //      }
+ //    }
+ //  }
+ //  if(homes.length > 0){
+ //    for(var rect in rectangles){
+ //      var coords = rectangles[rec]["coord"];
+ //      var northwest = coords[0];
+ //      var southeast = coords[1];
+ //      for(var h in homes){
+ //        var hNW = homes[h][0];
+ //        var nSE = homes[h][1];
+ //        var overlapping = (northwest[1] > hNW[1] && hNW[1] > southeast[1]) || (northwest[1] < hNW[1] && hSE[1] < northwest[1]);
+ //        if(!overlapping){
+ //          rectangles.splice(rect, 1);
+ //        }
+ //      }
+ //    }
+ //  }
+ //  getAggregate(count_dict);
+ //  return rectangles;
+ // }
 
 
 /**
  * Compares two arrays if they have the same values or not
  */
-function isSameSet(arr1, arr2){
-  return  $(arr1).not(arr2).length === 0 && $(arr2).not(arr1).length === 0;
-}
+// function isSameSet(arr1, arr2){
+//   return  $(arr1).not(arr2).length === 0 && $(arr2).not(arr1).length === 0;
+// }
--- maphomes.js
+++ maphomes.js
@@ -1,9 +1,56 @@
-var canvas, stage, image, bitmap;
-var homes = [];
+/* Initialize map variables */
+var canvas, stage, image, bitmap, map_container, homes_container, point_container, alpha, alphamask, alphastage;
 
-var places = [];
-var topX, topY, botX, botY, label;
+/* Initialize filter variables */
+var point = [];
+var homes = [];
+var topX, topY, botX, botY;
 var sliderRange = [1, 8];
+var toggle = "nothing";
+
+/* initialize on page load */
+function init() {
+  toQuad();
+
+  alpha = 0.5;
+  alphamask = document.getElementById("alphamask");
+  alphastage = new createjs.Stage(alphamask);
+
+	canvas = document.getElementById("maphomes");
+
+	stage = new createjs.Stage(canvas);
+  map_container = new createjs.Container();
+  homes_container = new createjs.Container();
+  point_container = new createjs.Container();
+  stage.addChild(map_container, homes_container, point_container);
+  var bg = new createjs.Shape();
+  bg.graphics.beginFill("#222");
+  bg.graphics.drawRect(0, 0, 3675, 4200);
+  map_container.addChild(bg);
+
+  image = new Image();
+  image.src = "birdseye.png";
+  bitmap = new createjs.Bitmap(image);
+
+  setTimeout(function () {
+    map_container.addChild(bitmap);
+    stage.update();
+  }, 1500);
+
+  stage.removeAllEventListeners();
+
+  stage.update();
+}
+
+
+/* Repositions screen to quad*/
+function toQuad() {
+  $(window).scrollTop(1200 - window.innerHeight / 2);
+  $(window).scrollLeft(1900 - window.innerWidth / 2 + 250);
+}
+
+
+/* Create homes rectangle */
 function handleMouseDown(event) {
   topX = stage.mouseX;
   topY = stage.mouseY;
@@ -11,18 +58,14 @@ function handleMouseDown(event) {
   botY = stage.mouseY;
 
   stage.removeAllEventListeners("stagemousedown");
-  stage.addEventListener("stagemousedown", handleClick);
   stage.addEventListener("stagemouseup", handleMouseUp);
 }
 
 function handleMouseUp(event) {
 
   if (stage.mouseX === topX || stage.mouseY === topY) {
-
     // nothing (box is a line)
-
   } else {
-
     if (stage.mouseX > topX) {
       botX = stage.mouseX;
     } else {
@@ -33,111 +76,58 @@ function handleMouseUp(event) {
     } else {
       topY = stage.mouseY;
     }
-
-    var g = new createjs.Graphics();
-    g.beginFill(createjs.Graphics.getRGB(212,93,0, 0.3));
-    g.drawRect(topX, topY, botX - topX, botY - topY);
-
-    var s = new createjs.Shape(g);
-
-    places.push([[topX, topY], [botX, botY]]);
-
-    stage.addChild(s);
-    stage.update();
   }
 
+  homes.push([[topX, topY],[botX, botY]]);
+  recalculate();
   stage.removeAllEventListeners("stagemouseup");
-  stage.addEventListener("stagemousedown", handleClick);
   stage.addEventListener("stagemousedown", handleMouseDown);
 }
 
-function handleCancel(event) {
-  stage.removeChildAt(stage.numChildren - 1);
-  stage.update();
-}
 
-function toQuad() {
-  $(window).scrollTop(1200 - window.innerHeight / 2);
-  $(window).scrollLeft(1900 - window.innerWidth / 2 + 250);
-}
-
-function init() {
-  toQuad();
-
-	canvas = document.getElementById("maphomes");
-
-	stage = new createjs.Stage(canvas);
-
-  image = new Image();
-  image.src = "birdseye.png";
-  bitmap = new createjs.Bitmap(image);
-
-  setTimeout(function () {
-    stage.addChild(bitmap);
-    stage.update();
-  }, 1500);
-
-  stage.removeAllEventListeners();
-  stage.addEventListener("stagemousedown", handleMouseDown);
-  stage.addEventListener("stagemousedown", handleClick);
-  stage.update();
-
-}
-
+/* Creates point */
 function handleClick(event) {
-  var point = [];
+  point = [];
   point[0] = stage.mouseX;
   point[1] = stage.mouseY;
-  console.log(point);
-  getLabels(point);
+  point_container.removeAllChildren();
+  recalculate();
 }
 
+
+/* clears map except for background and map layers */
 function clearMap(){
-  for(var i = stage.numChildren - 1; i > 0; i--){
+  for(var i = stage.numChildren - 1; i > 1; i--){
     stage.removeChildAt(i);
   }
   stage.update();
 }
 
+/* user interface */
 $(function() {
-  $( "#clearhomes" )
-    .button()
-    .click(function() {
-      // TODO
-    });
-});
 
-function drawRectangles(rectangles){
-  clearMap();
-  var alpha = 0.99/rectangles.length;
-  for(var i in rectangles){
-    var rectangle = rectangles[i];
-    var northwest = rectangle["coord"][0];
-    var southeast = rectangle["coord"][1];
-    var topX = northwest[0];
-    var topY = northwest[1];
-    var botX = southeast[0];
-    var botY = southeast[1];
+  /* alpha slider */
+  $( "#alpha-range" ).slider({
+    min: 0,
+    max: 3,
+    value: 1,
+    step: 0.1,
+    slide: function( event, ui ) {
 
-    var g = new createjs.Graphics();
-    g.beginFill(createjs.Graphics.getRGB(96,110,178,alpha));
-    g.drawRect(topX, topY, botX - topX, botY - topY);
+      alpha = (ui.value) * (ui.value) * (ui.value) / 27;
+      console.log(alpha);
+      recalculate();
+    }
+  });
 
-    var s = new createjs.Shape(g);
-    stage.addChild(s);
-  }
-  stage.update();
-}
-
-
-$(function() {
-  $("#search").on("keydown", function(e) {
-    if(e.keyCode == 13){
-      var keyword = $("input[name=keyword]").val();
-      var rectangles = searchFilter(keyword);
-      drawRectangles(rectangles);
+  /* search box */
+  $("#search").on("keypress", function(e) {
+    if(e.keyCode == 13) {
+      recalculate();
     }
   })
+
+  /* semester slider */
   $( "#slider-range" ).slider({
     range: true,
     min: 0,
@@ -155,32 +145,183 @@ $(function() {
       } else {
         $( "#amount" ).html( ui.values[ 0 ] + "–" + ui.values[ 1 ] + " semesters");
       }
-      sliderRange[0] = ui.values[ 0 ];
-      sliderRange[1] = ui.values[ 1 ];
-      var colleges = $('input[type="checkbox"][name="colleges"]:checked').map(function(){
-        return this.value;
-      }).toArray();
-      var vehicles = $('input[type="checkbox"][name="vehicles"]:checked').map(function(){
-        return this.value;
-      }).toArray();
-      var rectangles = filteredRectangles(colleges, sliderRange, vehicles, homes);
-      drawRectangles(rectangles);
+
+      sliderRange[0] = ui.values[0];
+      sliderRange[1] = ui.values[1];
+
+      recalculate();
     }
   });
+
+  /* semester slider label */
   $( "#amount" ).val( $( "#slider-range" ).slider( "values", 0 ) +
     " - " + $( "#slider-range" ).slider( "values", 1 ) );
 
-  $('input[type="checkbox"][name="colleges"], input[type="checkbox"][name="vehicles"]').on('change',function(){
-    var colleges = $('input[type="checkbox"][name="colleges"]:checked').map(function(){
-      return this.value;
-    }).toArray();
-    var vehicles = $('input[type="checkbox"][name="vehicles"]:checked').map(function(){
-      return this.value;
-    }).toArray();
-
-    console.log(homes);
-    var rectangles = filteredRectangles(colleges, sliderRange, vehicles, homes);
-    drawRectangles(rectangles);
+  /* colleges and vehicles */
+  $('input[type="checkbox"][name="colleges"], input[type="checkbox"][name="vehicles"]').on('change',function() {
+    recalculate();
   });
 
+/* cursor toggles */
+  $( "#examine" )
+    .button()
+    .click(function() {
+      if (toggle !== "examine") {
+        $("#cursor .help-block").html("<b>Current mode: Examine point</b><br>Click and drag on the map to designate a point. Labels will be filtered by those touching that point. You can only designate one point at a time.")
+        $("#examine").addClass("current");
+        $("#examine").html("Clear point");
+        $("#living").removeClass("current");
+        $("#living").html("Home filter");
+        $("#nothing").removeClass("current");
+        stage.removeAllEventListeners("stagemousedown");
+        stage.addEventListener("stagemousedown", handleClick);
+      } else {
+        point_container.removeAllChildren();
+        point = [];
+        recalculate();
+      }
+      toggle = "examine";
+    });
 });
+
+$(function() {
+  $( "#living" )
+    .button()
+    .click(function() {
+      if (toggle !== "living") {
+        $("#cursor .help-block").html("<b>Current mode: Home filter</b><br>Click and drag on the map to create a rectangle. Labels will be filtered by people who have lived in that area. You can draw multiple rectangles.")
+        $("#examine").removeClass("current");
+        $("#examine").html("Examine point");
+        $("#living").addClass("current");
+        $("#living").html("Clear homes");
+        $("#nothing").removeClass("current");
+        stage.removeAllEventListeners("stagemousedown");
+        stage.addEventListener("stagemousedown", handleMouseDown);
+      } else {
+        homes_container.removeAllChildren();
+        homes = [];
+        recalculate();
+      }
+      toggle = "living";
+    });
+});
+
+$(function() {
+  $( "#nothing" )
+    .button()
+    .click(function() {
+      if (toggle !== "examine") {
+        $("#cursor .help-block").html("<b>Current mode: None</b><br>Click on a button below to change input modes.")
+        $("#examine").removeClass("current");
+        $("#examine").html("Clear point");
+        $("#living").removeClass("current");
+        $("#living").html("Home filter");
+        $("#nothing").addClass("current");
+        clearMap();
+        point = [];
+        homes = [];
+        recalculate();
+        stage.removeAllEventListeners("stagemousedown");
+      }
+      toggle = "none";
+    });
+});
+
+
+/* graphics */
+
+function drawFog(rectangles) {
+
+  alphastage.removeAllChildren();
+
+  var s = new createjs.Shape();
+
+  s.graphics.beginFill("rgba(0, 255, 0, 0.05)");
+  s.graphics.drawRect(0, 0, 3675, 4200);
+  alphastage.addChild(s);
+
+
+  for (var i in rectangles) {
+    var rectangle = rectangles[i];
+    var northwest = rectangle["coord"][0];
+    var southeast = rectangle["coord"][1];
+    var topX = northwest[0];
+    var topY = northwest[1];
+    var botX = southeast[0];
+    var botY = southeast[1];
+    var sh = new createjs.Shape();
+    sh.graphics.beginFill("rgba(0,0,255," + alpha + ")");
+    sh.graphics.drawRect(topX, topY, botX - topX, botY - topY);
+    alphastage.addChild(sh);
+  }
+
+  alphastage.update();
+
+  alphastage.cache(0, 0, 3675, 4200);
+
+  bitmap.filters = [new createjs.AlphaMaskFilter(alphastage.cacheCanvas)];
+  bitmap.cache(0, 0, 3675, 4200);
+}
+
+function drawFilter() {
+  homes_container.removeAllChildren();
+  point_container.removeAllChildren();
+
+  if (point.length > 0) {
+    var g = new createjs.Graphics();
+    g.beginFill("rgba(255,0,0,0.8)");
+    g.drawCircle(point[0], point[1], 15);
+    var s = new createjs.Shape(g);
+    point_container.addChild(s);
+  }
+
+  if (homes.length > 0) {
+    for (h in homes) {
+      var house = homes[h];
+      var northwest = house[0];
+      var southeast = house[1];
+      var topX = northwest[0];
+      var topY = northwest[1];
+      var botX = southeast[0];
+      var botY = southeast[1];
+
+      var s = new createjs.Shape();
+      s.graphics.beginFill("rgba(255,0,0,0.4)");
+      s.graphics.drawRect(topX, topY, botX - topX, botY - topY);
+      homes_container.addChild(s);
+    }
+  }
+
+}
+
+
+/* AlphaMaskFilter example */
+//
+// var stage;
+//
+//
+// function init() {
+//
+// 	image = new Image();
+// 	image.src = "http://www.harrisremovals.co.uk/wp-content/uploads/2014/07/Crunch-time-in-the-removals-game.jpg";
+//
+// 	stage = new createjs.Stage("testCanvas");
+//
+//     var box = new createjs.Shape();
+//       box.graphics.beginLinearGradientFill(["#000000", "rgba(0, 0, 0, 0)"], [0, 1], 0, 0, 100, 100)
+//       box.graphics.drawRect(0, 0, 100, 100);
+//       box.cache(0, 0, 100, 100);
+//
+//   var bmp = new createjs.Bitmap(image);
+//   bmp.filters = [
+//       new createjs.AlphaMaskFilter(box.cacheCanvas)
+//   ];
+//   bmp.cache(0, 0, 100, 100);
+//
+//
+//   var bmp2 = new createjs.Bitmap("http://www.pyromaniac.com/system/marquee_larges/608/original/crunch-time-large.jpg");
+//
+//     stage.addChild(bmp2);
+//     stage.addChild(bmp);
+//   stage.update();
+// }
